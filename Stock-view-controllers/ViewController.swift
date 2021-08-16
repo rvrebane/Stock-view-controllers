9a8dad2134ec6045925988c6a1f4c445877aba92
Rein Saetalu <reinukas1@gmail.com>
2021-08-16 21:00:04 +0300

    Initial Commit

---
+++ Stock-view-controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock-view-controllers
+//
+//  Created by Rein Saetalu on 16.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
