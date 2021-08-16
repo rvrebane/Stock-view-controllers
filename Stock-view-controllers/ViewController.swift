1074f7c76bacac6500fed3e5b877acbf906087f0
Rein Saetalu <reinukas1@gmail.com>
2021-08-16 21:13:29 +0300

    imagePicker launches

--- Stock-view-controllers/ViewController.swift
+++ Stock-view-controllers/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var presentButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Actions
+    
+    // Method to open ImagePickerController
+    @IBAction func experiment(sender: UIButton) {
+        let nextController = UIImagePickerController()
+        present(nextController, animated: true, completion: nil)
     }
 
 
